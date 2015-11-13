14cd192a973c982b0ef91b9a6d33332ee69a65e9
Larry Nickerson <lnickers@austin.rr.com>
2015-11-12 22:39:11 -0600

    attempting to get table to populate

--- Watch Extension/ScheduleInterfaceController.swift
+++ Watch Extension/ScheduleInterfaceController.swift
@@ -31,5 +31,52 @@ class ScheduleInterfaceController: WKInterfaceController {
         for flight in flights {
             NSLog("destination: \(flight.destination)")
         }
+        
+        // fill the flight table with data
+        for index in 0..<flightsTable.numberOfRows {
+        
+            // if the current row has a valid controller
+            if flightsTable.rowControllerAtIndex(index) == nil{
+                print(" nil")
+            }
+            if let controller = flightsTable.rowControllerAtIndex(index) as? FlightRowController
+            {
+            
+                //fill the row with flight details
+                controller.flight = flights[index]
+                print("yo!!!")
+            }
+        }
     }
+ 
+    
+// Tried this but to no avail...
+// http://stackoverflow.com/questions/29147205/apple-watch-table-first-4-rows-not-appearing
+//    override func willActivate() {
+//        // create an instance of the row you just built in Interface Builder for each flight in
+//        // flights. The number of rows is equal to the size of the array, and the row type
+//        // is the identifier you set in the storyboard
+//        flightsTable.setNumberOfRows(flights.count, withRowType: "FlightRow")
+//        NSLog("There are \(flights.count) flights.")
+//        
+//        for flight in flights {
+//            NSLog("destination: \(flight.destination)")
+//        }
+//        
+//        // fill the flight table with data
+//        for index in 0..<flightsTable.numberOfRows {
+//            
+//            // if the current row has a valid controller
+//            if flightsTable.rowControllerAtIndex(index) == nil{
+//                print(" nil")
+//            }
+//            if let controller = flightsTable.rowControllerAtIndex(index) as? FlightRowController
+//            {
+//                
+//                //fill the row with flight details
+//                controller.flight = flights[index]
+//                print("yo!!!")
+//            }
+//        }
+//    }
 }
