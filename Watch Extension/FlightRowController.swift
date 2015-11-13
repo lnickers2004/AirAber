114210de098a2b43e2ea112e5cc7ee6f02eff090
Larry Nickerson <lnickers@austin.rr.com>
2015-11-12 21:01:48 -0600

    added  FlightRowController class with outlet instance variables

---
+++ Watch Extension/FlightRowController.swift
@@ -0,0 +1,21 @@
+//
+//  FlightRowController.swift
+//  AirAber
+//
+//  Created by Larry Nickerson on 11/12/15.
+//  Copyright © 2015 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+
+class FlightRowController: NSObject {
+
+    @IBOutlet var separator: WKInterfaceSeparator!
+    @IBOutlet var originLabel: WKInterfaceLabel!
+    @IBOutlet var destinationLabel: WKInterfaceLabel!
+    @IBOutlet var flightNumberLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var planeImage: WKInterfaceImage!
+    
+    
+}
