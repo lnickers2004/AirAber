f92f896b275b6bbc532e73d0e17c2b8ae8c61477
Larry Nickerson <lnickers@austin.rr.com>
2015-11-12 19:59:51 -0600

    adding images

---
+++ Watch Extension/ScheduleInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  ScheduleInterfaceController.swift
+//  AirAber
+//
+//  Created by Larry Nickerson on 11/12/15.
+//  Copyright © 2015 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ScheduleInterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
